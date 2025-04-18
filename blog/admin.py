38f1121faf81dca5223e20363be93b310e7b2514
Partho Debnath <parthodebnath28@gmail.com>
2025-04-18 23:50:13 +0600

    Post model created and register in the admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "slug",
+        "author",
+        "publish",
+        "status",
+    ]
+    list_filter = [
+        "status",
+        "created",
+        "publish",
+        "author",
+    ]
+    search_fields = [
+        "title",
+        "body",
+        "author__email",
+    ]
+    search_help_text = "Search Post based on title or body or author-email"
+    prepopulated_fields = {
+        "slug": [
+            "title",
+        ],
+    }
+    raw_id_fields = [
+        "author",
+    ]
+    ordering = [
+        "publish",
+        "status",
+    ]
+    date_hierarchy = "publish"
+    show_facets = admin.ShowFacets.ALWAYS
+    # autocomplete_fields = [
+    #     "author",
+    # ]
