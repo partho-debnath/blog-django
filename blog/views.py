a942585126fc1d1ae47e57c3a206c34579dfcbe9
Partho Debnath <parthodebnath28@gmail.com>
2025-05-04 02:35:42 +0600

    pagination template added

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.http import HttpRequest
 
 from .models import Post
 
 
 def post_list(request: HttpRequest):
-    posts = Post.published.all()
+
+    post_list = Post.published.all()
+    paginator = Paginator(object_list=post_list, per_page=2)
+
+    page_number = request.GET.get("page", 1)
+    posts = paginator.get_page(page_number)
     return render(
         request=request,
         template_name="blog/post/list.html",
